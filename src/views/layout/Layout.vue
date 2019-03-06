85228ee7e5f0e0c0bd42734b75a4a1d595d36713
yingpengsha <810998652@qq.com>
2019-03-06 19:13:08 +0800

    增强样式管理

---
+++ src/views/layout/Layout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="layout">
+    <header>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Layout',
+};
+</script>
+
+<style lang="scss" scoped>
+#layout{
+
+}
+</style>
